52d57a15fd52842047ecf89c76e06797f1232dab
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-07-17 02:10:35 -0300

    implementa fluxo service, teste ok!

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -1,6 +1,9 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,12 @@ import jakarta.validation.Valid;
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
 	@PostMapping
-	@ResponseStatus(code=HttpStatus.CREATED)
+	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+
+	@GetMapping
+	@ResponseStatus(code = HttpStatus.OK)
+	List<ClienteListResponse> getTodosClientes();
 }
+
+
