bd10183cf25c9007a2ef668b3b043d80b9af40ae
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-08-09 10:13:18 -0300

    implementa conversao pet para petclienteresponse

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetAPI.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.petz.clientepet.cliente.application.api.ClienteDetalhadoResponse;
 import jakarta.validation.Valid;
 
 @RestController
@@ -25,5 +26,19 @@ public interface PetAPI {
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
 	List<PetClienteListResponse> getPetsDoClienteComId(@PathVariable UUID idCliente);
+
+	@GetMapping(value = "/{idPet}")
+	@ResponseStatus(code = HttpStatus.OK)
+	ClienteDetalhadoResponse getClienteAtravesId(@PathVariable UUID idCliente, @PathVariable UUID idPet);
+
+//	@DeleteMapping(value = "/{idCliente}")
+//	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+//	void deletaClienteAtravesId(@PathVariable UUID idCliente);
+//
+//	@PatchMapping(value = "/{idCliente}")
+//	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+//	void patchAlteraCliente(@PathVariable UUID idCliente,
+//	@Valid @RequestBody ClienteAlteracaoRequest clienteAlteracaoRequest);
+
 
 }
