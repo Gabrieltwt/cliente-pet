40b6801dc30b731d351fce091381b8b0dbd97395
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-08-21 21:59:37 -0300

    implementa fluxo da camada de API

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -31,5 +31,12 @@ public class PetController implements PetAPI {
 		log.info("[finaliza] PetController - getPetsDoClienteComId");
 		return petsDoCliente;
 	}
+	@Override
+	public PetClienteDetalheResponse getPetDoClienteComId(UUID idCliente, UUID idPet) {
+		log.info("[inicia] PetController - getPetDoClienteComId");
+		log.info("[idCliente] {} - [idPet] {}", idCliente, idPet);
+		log.info("[finish] PetController - getPetDoClienteComId");
+		return null;
+	}
 
 }
