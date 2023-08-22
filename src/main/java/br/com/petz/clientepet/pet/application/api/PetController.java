d6cadcad684760833859c500946c77af3402b20e
Gabrieltwt <gabriel.cunhaa.info@gmail.com>
2023-08-22 10:11:38 -0300

    implementa delete pet camada de api e fluxo de servico de aplicacao

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -39,5 +39,13 @@ public class PetController implements PetAPI {
 		log.info("[finish] PetController - getPetDoClienteComId");
 		return pet;
 	}
+	@Override
+	public void deletePetDoClienteComId(UUID idCliente, UUID idPet) {
+		log.info("[inicia] PetController - deletePetDoClienteComId");
+		log.info("[idCliente] {} - [idPet] {}", idCliente, idPet);
+		petService.deletaPetDoClienteComID(idCliente,idPet);
+		log.info("[finaliza] PetController - deletePetDoClienteComId");
+		
+	}
 
 }
